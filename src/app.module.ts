7a0f79f1bcfa2f28ded1627a12b5791503331286
Ovik Gukeyan <0953188061ovik@gmail.com>
2024-11-28 21:47:29 +0100

    env

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,20 +7,23 @@ import { FilesModule } from './files/files.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './users/entities/user.entity';
 import { FileEntity } from './files/entities/file.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: 'trumpet.db.elephantsql.com',
-    port: 5432,
-    username: 'abjeermx',
-    password: '0ROVPFGwGVRmsf1A6_oRhqwsiRo1sPS_',
-    database: 'abjeermx',
-    entities: [UserEntity, FileEntity],
-    synchronize: true,
-  }), 
-  UsersModule, FilesModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT) || 5432,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [UserEntity, FileEntity],
+      synchronize: true,
+    }),
+    UsersModule, FilesModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
